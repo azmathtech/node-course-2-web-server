dd533990cdb6299c028533456ed6c429676f08fa
Michael Evans <azmathtech@gmail.com>
2018-11-01 16:25:06 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 //hbs is handlebarjs which is a templeting helper to build pages
@@ -68,6 +69,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
